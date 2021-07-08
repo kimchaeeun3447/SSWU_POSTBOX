66dc763e542af5bd05ce90461852369cd70172c8
eunjungL <dldmswjd322@gmail.com>
2021-07-09 01:03:03 +0900

    signin xml almost complete

--- app/src/main/java/com/example/sswu_postbox/MainActivity.java
+++ app/src/main/java/com/example/sswu_postbox/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sswu_postbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signIn = findViewById(R.id.button);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
