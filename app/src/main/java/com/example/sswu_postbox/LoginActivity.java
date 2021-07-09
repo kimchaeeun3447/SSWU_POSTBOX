1950f2a66e58c26c99d710f1fdc0261c0fb997fb
eunjungL <dldmswjd322@gmail.com>
2021-07-10 00:28:10 +0900

    login activity signup btn connet

--- app/src/main/java/com/example/sswu_postbox/LoginActivity.java
+++ app/src/main/java/com/example/sswu_postbox/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.sswu_postbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button go_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        go_sign_up = findViewById(R.id.signup_btn);
+
+        go_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
