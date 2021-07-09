be3d54c09333b36c8898c5ed8c7dbfe8c03fe74e
eunjungL <dldmswjd322@gmail.com>
2021-07-10 00:01:52 +0900

    add sign up activity

---
+++ app/src/main/java/com/example/sswu_postbox/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.sswu_postbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
