3ed0e5a88fe9122cb626f854d0d593ab63123041
kimchaeeun3447 <rlacodms3447@naver.com>
2021-07-13 22:24:43 +0900

    [xml 파일 추가] 키워드 설정, 키워드 별 공지확인, 공지 클릭 시 연결 페이지, 알림창 xml
    * 키워드 설정
     - KeywordSetting
    * 키워드 별 공지확인
     - CheckKeywordPost
    * 공지 클릭 시 연결 페이지
     - PostClick
    * 알림창
    - NotificationList

---
+++ app/src/main/java/com/example/sswu_postbox/CheckKeywordPostActivity.java
@@ -0,0 +1,14 @@
+package com.example.sswu_postbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckKeywordPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_keyword_post);
+    }
+}
---
+++ app/src/main/java/com/example/sswu_postbox/NotificationListActivity.java
@@ -0,0 +1,14 @@
+package com.example.sswu_postbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_list);
+    }
+}
