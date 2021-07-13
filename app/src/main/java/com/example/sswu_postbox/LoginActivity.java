c5e065ff9a24abde60acdc77a5b57c22aee79538
eunjungL <dldmswjd322@gmail.com>
2021-07-14 01:08:45 +0900

    build.gradle volley add

--- app/src/main/java/com/example/sswu_postbox/LoginActivity.java
+++ app/src/main/java/com/example/sswu_postbox/LoginActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button go_sign_up;
+    Button go_sign_up, sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +17,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         go_sign_up = findViewById(R.id.signup_btn);
+        sign_in = findViewById(R.id.login_btn);
 
         go_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(i);
+            }
+        });
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
