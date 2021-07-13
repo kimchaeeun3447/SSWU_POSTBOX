e43f709acff71df99fc1a4035400021f182990d5
eunjungL <dldmswjd322@gmail.com>
2021-07-14 01:56:35 +0900

    LoginActivity.java login implement

--- app/src/main/java/com/example/sswu_postbox/LoginActivity.java
+++ app/src/main/java/com/example/sswu_postbox/LoginActivity.java
@@ -3,13 +3,33 @@ package com.example.sswu_postbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String TAG = LoginActivity.class.getSimpleName();
+
     Button go_sign_up, sign_in;
+    EditText id, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +50,54 @@ public class LoginActivity extends AppCompatActivity {
         sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                login();
             }
         });
+    }
+
+    void login() {
+        String url = "http://3.37.68.242:8000/login/";
+
+        id = findViewById(R.id.login_id_edit);
+        password = findViewById(R.id.login_pwd_edit);
+
+        HashMap<String, String> login_json = new HashMap<>();
+        login_json.put("username", id.getText().toString());
+        login_json.put("password", password.getText().toString());
+
+        JSONObject parameter = new JSONObject(login_json);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST,
+                url,
+                parameter,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String access_token = response.getString("access");
+
+                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                            editor.putString("access_token", access_token);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Toast toast = Toast.makeText(getApplicationContext(), "환영합니다.", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Log.d(TAG, "login fail");
+                    }
+                });
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
 }
