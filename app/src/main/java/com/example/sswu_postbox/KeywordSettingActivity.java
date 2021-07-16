50b801d1b5eaa185913a0316746a898638b39952
eunjungL <dldmswjd322@gmail.com>
2021-07-16 16:49:01 +0900

    KeywordSettingActivity.java keyword list floating implement complete

--- app/src/main/java/com/example/sswu_postbox/KeywordSettingActivity.java
+++ app/src/main/java/com/example/sswu_postbox/KeywordSettingActivity.java
@@ -15,27 +15,28 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,16 +46,18 @@ public class KeywordSettingActivity extends AppCompatActivity {
     EditText keyword_add_text, keyword_del_text;
     Button keyword_add_btn, keyword_del_btn;
 
+    static String[] list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyword_setting);
 
-
         final GridView my_keyword_list = (GridView)findViewById(R.id.my_keyword_list);
         MyGridAdapter gridAdapter = new MyGridAdapter(this);
         my_keyword_list.setAdapter(gridAdapter);
 
+        keyword_list();
 
         keyword_add_btn = findViewById(R.id.keyword_adding_btn);
         keyword_add_btn.setOnClickListener(new View.OnClickListener() {
@@ -79,13 +82,7 @@ public class KeywordSettingActivity extends AppCompatActivity {
         Context context;
 
         //사용자가 등록한 모든 키워드들을 받아올 변수(임시로 텍스트 넣어둠)
-        String[] user_keyword_list = {"one", "two", "three",
-                "four", "five","six",
-                "7","8","9",
-                "10","11","12",
-                "13","14","15",
-        "16","17","18"};
-
+        static String[] user_keyword_list = { "A" };
 
         public MyGridAdapter(Context c){
             context = c;
@@ -123,18 +120,14 @@ public class KeywordSettingActivity extends AppCompatActivity {
             textView.setText(user_keyword_list[position]);
 
             return textView;
-
-
-
         }
     }
 
-
     void keyword_add() {
-        keyword_add_text = findViewById(R.id.keyword_adding);
-
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String token = sharedPreferences.getString("access_token", "null");
+
+        keyword_add_text = findViewById(R.id.keyword_adding);
 
         HashMap<String, String> keywordAdd_json = new HashMap<>();
         keywordAdd_json.put("keyword", keyword_add_text.getText().toString());
@@ -174,10 +167,10 @@ public class KeywordSettingActivity extends AppCompatActivity {
     }
 
     void keyword_del() {
-        keyword_del_text = findViewById(R.id.keyword_delete);
-
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String token = sharedPreferences.getString("access_token", "null");
+
+        keyword_del_text = findViewById(R.id.keyword_delete);
 
         String url = "http://3.37.68.242:8000/detail/keywords/?keyword=" + keyword_del_text.getText().toString();
 
@@ -211,12 +204,58 @@ public class KeywordSettingActivity extends AppCompatActivity {
         queue.add(stringRequest);
     }
 
-    Map<String, String> give_token(String token) {
+    void keyword_list() {
+        String TAG = KeywordSettingActivity.class.getSimpleName();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String token = sharedPreferences.getString("access_token", "null");
+
+        String url = "http://3.37.68.242:8000/detail/keywords/";
+
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        ArrayList<String> keyword_list = new ArrayList<>();
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                keyword_list.add(response.getJSONObject(i).getString("keyword"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        MyGridAdapter.user_keyword_list = keyword_list.toArray(new String[keyword_list.size()]);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "keyword list get fail");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return give_token(token);
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+     Map<String, String> give_token(String token) {
         HashMap<String, String> headers = new HashMap<>();
         headers.put("Authorization", "Bearer " + token);
 
         return headers;
     }
 }
+
+
 
 
