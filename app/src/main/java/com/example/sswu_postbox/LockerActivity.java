cae87bb239290b34e2616c30e81721d4fccd2c63
kimchaeeun3447 <rlacodms3447@naver.com>
2021-07-18 21:06:13 +0900

    [Style] 보관함, 알림창 back 버튼

--- app/src/main/java/com/example/sswu_postbox/LockerActivity.java
+++ app/src/main/java/com/example/sswu_postbox/LockerActivity.java
@@ -3,6 +3,8 @@ package com.example.sswu_postbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LockerActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class LockerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locker);
+
+
+        ImageButton locker_back_btn = findViewById(R.id.locker_back_btn);
+        locker_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/sswu_postbox/NotificationListActivity.java
+++ app/src/main/java/com/example/sswu_postbox/NotificationListActivity.java
@@ -2,7 +2,11 @@ package com.example.sswu_postbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class NotificationListActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class NotificationListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_list);
+
+
+        ImageButton notification_list_back_btn = findViewById(R.id.notification_list_back_btn);
+        notification_list_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
