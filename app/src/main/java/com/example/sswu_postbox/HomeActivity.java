79a8aeb938a27d4439bb0b7ab3ab916c34dc7283
kimchaeeun3447 <rlacodms3447@naver.com>
2021-07-18 21:03:53 +0900

    [Style] 홈화면 하단 메뉴 이미지 추가, 홈화면 버튼 엑티비티 연결

--- app/src/main/java/com/example/sswu_postbox/HomeActivity.java
+++ app/src/main/java/com/example/sswu_postbox/HomeActivity.java
@@ -2,7 +2,11 @@ package com.example.sswu_postbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,53 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        Button plus_keyword_btn = findViewById(R.id.my_keyword_list_plus_btn);
+        plus_keyword_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), KeywordSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button check_keyword_post_btn = findViewById(R.id.check_keyword_post_btn);
+        check_keyword_post_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CheckKeywordPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ImageButton notification_btn = findViewById(R.id.notification_btn);
+        notification_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NotificationListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ImageButton locker_btn = findViewById(R.id.locker_btn);
+        locker_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LockerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
