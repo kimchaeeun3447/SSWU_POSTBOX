1334c6abf0c55a253ba65c0e9459c10e92f8aa1e
eunjungL <dldmswjd322@gmail.com>
2021-08-02 17:12:02 +0900

    LockerActivity.java Locker implement

--- app/src/main/java/com/example/sswu_postbox/LockerActivity.java
+++ app/src/main/java/com/example/sswu_postbox/LockerActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -19,20 +20,31 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LockerActivity extends AppCompatActivity {
+    String TAG = LockerActivity.class.getSimpleName();
 
     GridView my_keyword_list;
     MyGridAdapter gridAdapter;
+
+    //보관된 공지사항 listview
+    ListView postList;
+    ArrayList<String> post_title = new ArrayList<>();
+    ArrayList<String> post_date = new ArrayList<>();
+    ArrayList<Boolean> post_saved = new ArrayList<>();
+    MyListAdapter myListAdapter;
 
     private BottomNavigationView bottomNavigationView;
 
@@ -46,6 +58,12 @@ public class LockerActivity extends AppCompatActivity {
         my_keyword_list.setAdapter(gridAdapter);
 
         keyword_list();
+
+        postList = findViewById(R.id.locker_post_listView);
+        myListAdapter = new MyListAdapter(this, post_title, post_date, post_saved);
+        postList.setAdapter(myListAdapter);
+
+        notice_list();
 
         ImageButton locker_back_btn = findViewById(R.id.locker_back_btn);
         locker_back_btn.setOnClickListener(new View.OnClickListener() {
@@ -111,6 +129,51 @@ public class LockerActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d(TAG, "keyword list get fail");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                return give_token(token);
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    void notice_list() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String token = sharedPreferences.getString("access_token", "null");
+
+        String url = "http://3.37.68.242:8000/stored/notice/";
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject notice = response.getJSONObject(i).getJSONObject("notice");
+                                post_title.add(notice.getString("title"));
+                                post_date.add(notice.getString("date"));
+
+                                JSONObject user_notice = response.getJSONObject(i);
+                                post_saved.add(user_notice.getBoolean("store"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        myListAdapter.notifyDataSetChanged();
+                    }
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Log.d(TAG, "notice user list error");
                     }
                 }) {
             @Override
