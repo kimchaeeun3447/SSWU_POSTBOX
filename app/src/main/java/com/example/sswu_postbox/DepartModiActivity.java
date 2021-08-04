23c42f69bd4a2a331c72ee2e6743e794694d4b06
zinzo1019 <topjoy22@naver.com>
2021-08-04 21:22:27 +0900

    과 수정하는 거 이제부터 구현해야 함

---
+++ app/src/main/java/com/example/sswu_postbox/DepartModiActivity.java
@@ -0,0 +1,14 @@
+package com.example.sswu_postbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepartModiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depart_modi);
+    }
+}
