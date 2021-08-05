7f0c4c6a92c958200c690d56e4491391c55346a7
zinzo1019 <topjoy22@naver.com>
2021-08-05 16:37:11 +0900

    학과 수정 창 xml과 액티비티

--- app/src/main/java/com/example/sswu_postbox/DepartModiActivity.java
+++ app/src/main/java/com/example/sswu_postbox/DepartModiActivity.java
@@ -3,6 +3,8 @@ package com.example.sswu_postbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DepartModiActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class DepartModiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_depart_modi);
+
+        ImageButton modi_back_btn = findViewById(R.id.modi_back_btn);
+        modi_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
